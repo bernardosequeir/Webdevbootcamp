5cc318ffbe06b76371580d90a280e6dd53cac14e
= <bernacsequeira@gmail.com>
2018-02-03 15:47:37 +0000

    Finished todo project, started patatap clone

--- toDoListProject/assets/js/todos.js
+++ toDoListProject/assets/js/todos.js
@@ -1,6 +1,25 @@
 //Check off specific todos with clicks
+$("ul").on('click', "li", function(){
+   $(this).toggleClass("completed");
+});   
+// Click X to delete Todo
+$("ul").on("click","span",function(event){
+$(this).parent().fadeOut(500, function(){
+    $(this).remove();
+});
+event.stopPropagation();
+});
 
-$("li").click(function(){
-    $(this).css("color","gray");
-    $(this).css("text-decoration","line-through");
+// Adding Todos
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val(); 
+        $(this).val("");
+        $("ul").append("<li><span><i class=\"fas fa-trash\"></i></span>" + todoText +"</li>");        
+      }
+});
+
+
+$(".fa-pencil-alt").on("click","span",function(event){
+    $("input[type='text']").fadeToggle();
 });
